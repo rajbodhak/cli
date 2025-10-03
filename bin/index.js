4c9c8eb2cf5d16a51cb8f36350fd85b2561d4daf
Raj Bodhak <rajlaptop2004@gmail.com>
2025-10-03 18:29:47 +0530

    Exploring chalk and inquirer

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,21 @@
 #!/usr/bin/env node
 
 import { program } from "commander";
+import chalk from "chalk";
+import inquirer from "inquirer";
 
-program
-    .version("1.0.0")
-    .description("This is my test cli")
-    .option("-n, --name <type>", "Say Your Name")
-    .action((option) => {
-        console.log(`Hey, ${option.name}`);
-    });
+program.version("1.0.0").description("This is my test cli");
+
+program.action(() => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ]).then((message) => {
+        console.log(chalk.blue(`Hello! nice to meet you ${chalk.yellow(message.name)}`))
+    })
+})
 
 program.parse(process.argv);
